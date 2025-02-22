8730654b2c39304c2b3d85f421dce6f5f2c5c65d
ZMegX <picardomegan@gmail.com>
2025-02-22 13:50:18 +0000

    main-home script file created and uploaded

---
+++ JS/main-home.js
@@ -0,0 +1,57 @@
+let slideIndex = 1;
+      showSlides(slideIndex);
+
+    // Next/previous controls
+    function plusSlides(n) {
+      showSlides(slideIndex += n);
+    }
+
+    // Thumbnail image controls
+    function currentSlide(n) {
+      showSlides(slideIndex = n);
+    }
+
+    function showSlides(n) {
+      let i;
+      let slides = document.getElementsByClassName("mySlides");
+      let dots = document.getElementsByClassName("demo");
+      let captionText = document.getElementById("caption");
+      if (n > slides.length) {slideIndex = 1}
+      if (n < 1) {slideIndex = slides.length}
+      for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+      }
+      for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+      }
+      slides[slideIndex-1].style.display = "block";
+      dots[slideIndex-1].className += " active";
+      captionText.innerHTML = dots[slideIndex-1].alt;
+    } 
+
+    document.addEventListener("keydown", (event) => {
+      if (event.key === "ArrowRight") {
+        plusSlides(1);
+      } else if (event.key === "ArrowLeft") {
+        plusSlides(-1);
+      }
+    });
+    const animation_elements = document.querySelectorAll('.animate-on-scroll');
+
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add('animate');
+        } else {
+          entry.target.classList.toggle('animate');
+        }
+      })
+    }, {
+      threshold: 0.5
+    });
+
+    for (let i=0; i < animation_elements.length; i++) {
+      const el = animation_elements[i];
+
+      observer.observe(el);
+    };
